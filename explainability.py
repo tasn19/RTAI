fe4c9d4fa8aa1d453acc317e7b729700aead240b
Romina <rominaabadi1994@gmail.com>
2022-02-10 11:04:56 -0500

    explainability updated

--- explainability.py
+++ explainability.py
@@ -10,7 +10,9 @@ from Models.C19Xception import C19Xception
 from data import CovidDataSet
 from data import MyTopCropTransform
 
-sys.path.insert(1, "./grad-cam-pytorch-master")
+import cv2
+
+sys.path.insert(1, "./grad-cam-pytorch-master")  # TODO: make it universal (windows addresses)
 import grad_cam
 
 device = "cpu"
@@ -35,10 +37,8 @@ model.eval()
 
 test_dataset = CovidDataSet(test_metadata_path, test_images_path, test_transform)
 test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False,
-                                 num_workers=0)
+                             num_workers=0)
 
-
-ims, labels = next(iter(test_dataloader))
 gcam = grad_cam.GradCAM(model=model)
 
 # probs, ids = gcam.forward(ims)
@@ -47,18 +47,29 @@ gcam = grad_cam.GradCAM(model=model)
 # print(gcam.fmap_pool.keys())
 # print(gcam.grad_pool.keys())
 
-target_layers = ["backbone.conv1", 'backbone.bn2']
+target_layers = dict(model.named_modules()).keys()
+target_layers = ['backbone.conv1', 'backbone.conv4']
 
-for target_layer in target_layers:
+im_num = 5
 
-    probs, ids = gcam.forward(ims)
-    gcam.backward(ids=torch.tensor([[0]]))
-    regions = gcam.generate(target_layer=target_layer)
+for i, (im, labels) in enumerate(test_dataloader):
+    if i >= im_num:
+        break
+    for target_layer in target_layers:
+        probs, ids = gcam.forward(im)
+        gcam.backward(ids=ids)
+        regions = gcam.generate(target_layer=target_layer)
+        # regions = regions/max(regions)
 
-    # plt.imshow(ims.permute(2,3,1,0).squeeze())
-    # plt.imshow(regions.permute(2,3,1,0).squeeze(), cmap='Reds', alpha=0.5)
-    # plt.show()
+        # plt.imshow(ims.permute(2,3,1,0).squeeze())
+        # plt.imshow(regions.permute(2,3,1,0).squeeze(), cmap='Reds', alpha=0.5)
+        # plt.show()
 
-    plt.subplot(1,2,1).imshow(ims.permute(2,3,1,0).squeeze())
-    plt.subplot(1,2,2).imshow(regions.permute(2,3,1,0).squeeze(), cmap='jet')
-    plt.show()
+        im2 = im.permute(2, 3, 1, 0).squeeze()
+        regions = regions.permute(2, 3, 1, 0).squeeze()
+        cmap = plt.get_cmap('jet')
+        combined = 0.5 * im2 + 0.5 * cmap(regions)[:, :, :3]
+        plt.subplot(1, 3, 1).imshow(im2)
+        plt.subplot(1, 3, 2).imshow(regions, cmap='jet')
+        plt.subplot(1, 3, 3).imshow(combined)
+        plt.show()
